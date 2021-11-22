79d8dc39ae480cbd6b74f16353c3e530d2d704d4
Andres Malagon <andresmalagon@Andress-MacBook-Pro.local>
2021-11-22 01:11:28 -0500

    finished the prompts

--- index.js
+++ index.js
@@ -1,61 +1,216 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
 
 const teamMembers = [];
 
 
-function init() {
-    console.log("Welcome to Team Generator!");
-    inquirer
-        .prompt([
+// function init() {
+//     console.log("Welcome to Team Generator!");
 
-            {
-                type: 'input',
-                name: 'name',
-                message: "What is the employee's name?",
-                validate: answer => {
-                    if (!answer) {
-                        return "You must enter your name."
-                    }
-                    return true;
+
+
+const managerPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the manager's name?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter a name."
                 }
-
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: "What is the employee's ID?",
-                validate: answer => {
-                    if (!answer) {
-                        return "You must enter the employee's ID."
-                    }
-                    return true;
-                }
-
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: "What is the employee's email?",
-                validate: answer => {
-                    if (!answer) {
-                        return "You must enter the employee's email."
-                    }
-                    return true;
-                }
-
+                return true;
             }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's ID?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter an ID."
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email address?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter an email address."
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the manager's office number?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter a number."
+                }
+                return true;
+            }
+        },
 
 
-
-        ])
-        .then(answers => {
-            console.log(answers);
-        })
+    ]).then(answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        teamMembers.push(manager);
+        chooseTeam()
+    })
 
 }
+managerPrompt()
 
+const chooseTeam = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "teamMember",
+            message: "Would you like to add a team member to the project?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "No thanks, I'm finished building the team."
+            ]
+        }
+
+    ]).then(answers => {
+        switch (answers.teamMember) {
+            case "Engineer":
+                engineerPrompt();
+                break;
+            case "Intern":
+                internPrompt();
+                break;
+            default: console.log(teamMembers);
+        }
+    })
+}
+
+const engineerPrompt = () => {
+    return inquirer.prompt([
+
+        //questions about employees
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter the employee's name."
+                }
+                return true;
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's ID?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter the employee's ID."
+                }
+                return true;
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the employee's email?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter the employee's email."
+                }
+                return true;
+            }
+
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the employee's Github username?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter the employee's Github username."
+                }
+                return true;
+            }
+        }
+    ]).then(answers => {
+        const engineer = new Engineer(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNum);
+        teamMembers.push(engineer);
+        chooseTeam()
+    })
+}
+
+const internPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the intern's name?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter the intern's name."
+                }
+                return true;
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the intern's ID?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter the intern's ID."
+                }
+                return true;
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the intern's email?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter the intern's email."
+                }
+                return true;
+            }
+
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is the intern's school?",
+            validate: answer => {
+                if (!answer) {
+                    return "You must enter the intern's school."
+                }
+                return true;
+            }
+
+        },
+
+    ]).then(answers => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        teamMembers.push(intern)
+        chooseTeam()
+    })
+}
 
 
 
@@ -67,4 +222,4 @@ function init() {
 //     });
 // };
 
-init();
+// init();
